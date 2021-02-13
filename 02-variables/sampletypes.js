5f1090b8086527b17c0e226208708d3b15fb5267
mari <mari20201101@gmail.com>
2021-02-13 12:38:24 +0900

    first commit

---
+++ 02-variables/sampletypes.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 99.9;
+var firstname = "mari";
+var lastname = "kuga";
+console.log(found);
+console.log("Grade is " + grade);
+console.log("Name is " + firstname + " " + lastname + ".");
---
+++ 02-variables/sampletypes.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 99.9;
+let firstname : string = "mari";
+let lastname : string = "kuga";
+
+console.log(found);
+console.log("Grade is " + grade);
+console.log(`Name is ${firstname } ${lastname}.`); 
+
+// found = 0;
